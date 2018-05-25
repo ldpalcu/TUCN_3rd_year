49f98e5387b2fedcdf827ff6422c89878b976f3b
LDP <ldpalcu@gmail.com>
2018-05-26 00:22:08 +0300

    update Lab2

--- SSC/Labs/Lab02/implementation/conv_BCD/conv_BCD_tb.vhd
+++ SSC/Labs/Lab02/implementation/conv_BCD/conv_BCD_tb.vhd
@@ -31,7 +31,55 @@ begin
                 BCD0 => BCD0);
     --generare vectorilor de test si aplicarea acestora la intrari
     gen_vect_test: process
-    variable VecTest    : STD_LOGIC_VECTOR(
+    variable VecTest   : STD_LOGIC_VECTOR(3 downto 0);
+    variable RezCorect : STD_LOGIC_VECTOR(7 downto 0);
+    variable NrErori   : INTEGER := 0;
+    begin
+        VecTest := x"0";
+        
+        for i in 0 to 15 loop
+            bin_number <= VecTest;
+            wait for 10 ns;
+            
+            if VecTest > x"9" then
+                RezCorect(7 downto 4) := x"1";
+                RezCorect(3 downto 0) := VecTest - x"9" - x"1";
+            else
+                RezCorect(7 downto 4) := x"0";
+                RezCorect(3 downto 0) := VecTest;
+            end if;
+            
+            if ((BCD1 & BCD0) /= RezCorect) then
+                report " Rezultat asteptat BCD1" &
+                    STD_LOGIC'image(RezCorect(7)) &
+                    STD_LOGIC'image(RezCorect(5)) &
+                    STD_LOGIC'image(RezCorect(6)) &
+                    STD_LOGIC'image(RezCorect(4)) &
+                    " Rezultat asteptat BCD0" &
+                    STD_LOGIC'image(RezCorect(3)) &
+                    STD_LOGIC'image(RezCorect(2)) &
+                    STD_LOGIC'image(RezCorect(1)) &
+                    STD_LOGIC'image(RezCorect(0)) &
+                    " /=Valoarea obtinuta " &
+                    STD_LOGIC'image(BCD1(3)) &
+                    STD_LOGIC'image(BCD1(2)) &
+                    STD_LOGIC'image(BCD1(1)) &
+                    STD_LOGIC'image(BCD1(0)) &
+                    STD_LOGIC'image(BCD0(3)) &
+                    STD_LOGIC'image(BCD0(2)) &
+                    STD_LOGIC'image(BCD0(1)) &
+                    STD_LOGIC'image(BCD0(0)) &
+                    " la momentul t = " &
+                    TIME'image (now)
+                    severity ERROR;
+                NrErori := NrErori + 1;
+             end if;
+             VecTest := VecTest + 1;
+         end loop;
+         report "Testare terminata cu " &
+          INTEGER'image (NrErori) & " erori";
+          wait;    
+    end process;
     
 
 
